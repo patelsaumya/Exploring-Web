6daba65132c23f079f647e87a27d62cbd520fb08
patelsaumya <saumyapatel.sap@gmail.com>
2023-05-31 13:27:13 +0530

    Initial Commit

--- JavaScript HTML5 APIs/project-3/src/index.js
+++ JavaScript HTML5 APIs/project-3/src/index.js
@@ -1,16 +1,27 @@
 import '../assets/css/style.css';
 import autumnMp4 from '../assets/media/autumn.mp4';
+import autumnMp3 from '../assets/media/autumn.mp3';
 
 import playIcon from '../assets/img/icons/play.svg';
 import pauseIcon from '../assets/img/icons/pause.svg';
 import stopIcon from '../assets/img/icons/stop.svg';
+import volumeIcon from '../assets/img/icons/volume.svg';
+import volumeMuteIcon from '../assets/img/icons/volume-mute.svg';
+import pipIcon from '../assets/img/icons/popup.svg';
 
 
 const app = document.getElementById('app');
 app.innerHTML = `
     <h1>JavaScript HTML5 APIs</h1>
     <div class="player">
+        <!--
+        <audio class="media" controls>
+            <source src="${autumnMp3}" type="audio/mpeg">
+        </audio>
+        -->
+
         <!-- <video autoplay loop> -->
+        
         <video class="media" controls>
             <source src="${autumnMp4}" type="video/mp4">
             <p>
@@ -26,6 +37,17 @@ app.innerHTML = `
             <button type="button" class="player__stop" aria-label="Stop">
                 <img src="${stopIcon}" alt="Stop Icon">
             </button>
+            <div class="player__progress">
+                <input type="range" class="player__timeline" min="0" max="100" value="0">
+                <span class="player__duration"></span>
+            </div>
+            <button type="button" class="player__pip" aria-label="Picture-in-Picture">
+                <img src="${pipIcon}" alt="Picture-in-Picture Icon">
+            </button>
+            <div class="player__sound">
+                <img src="${volumeIcon}" class="player__mute" alt="Volume Icon">
+                <input type="range" class="player__volume" min="0" max="100" value="100">
+            </div>
         </div>
     </div>
 `;
@@ -35,6 +57,14 @@ const play = player.querySelector('.player__play');
 const playImg = play.querySelector('img');
 const stop = player.querySelector('.player__stop');
 const media = player.querySelector('.media');
+// const media = new Audio(autumnMp3);
+const timeline = player.querySelector('.player__timeline');
+const duration = player.querySelector('.player__duration');
+const volume = player.querySelector('.player__volume');
+const volumeToggle = player.querySelector('.player__mute');
+const pip = player.querySelector('.player__pip');
+
+let totalDuration;
 
 // media.addEventListener('durationchange', (e) => {
 //     console.log(e.target.duration);
@@ -78,5 +108,71 @@ const stopMedia = () => {
     playImg.src = playIcon;
 }
 
+const getTime = (duration) => {
+    const time = parseInt(duration.toFixed(), 10);
+    const minutes = `${parseInt(time / 60)}`.padStart(2, 0);
+    const seconds = `${time % 60}`.padStart(2, 0);
+    return `${minutes}:${seconds}`;
+}
+
+const setInitialDuration = (e) => {
+    totalDuration = getTime(e.target.duration);
+    duration.innerText = `00:00 / ${totalDuration}`;
+}
+
+const setDuration = (e) => {
+    const currentDuration = getTime(e.target.currentTime);
+    const progress = (e.target.currentTime / e.target.duration) * 100;
+    timeline.value = progress;
+    duration.innerText = `${currentDuration} / ${totalDuration}`;
+}
+
+const skipToPosition = (e) => {
+    const position = parseInt(e.target.value, 10) / 100;
+    media.currentTime = media.duration * position;
+}
+
+const setVolume = (e) => {
+    const position = parseInt(e.target.value, 10) / 100;
+    media.volume = position;
+    volumeToggle.src = media.volume > 0 ? volumeIcon : volumeMuteIcon;
+}
+
+const toggleVolume = () => {
+    const isMuted = media.volume === 0;
+    volumeToggle.src = isMuted ? volumeIcon : volumeMuteIcon;
+    volume.value = isMuted ? 100 : 0;
+    volume.dispatchEvent(new Event('input'));
+    // media.volume = isMuted ? 1 : 0;
+}
+
 play.addEventListener('click', toggleMediaStatus);
 stop.addEventListener('click', stopMedia);
+
+media.addEventListener('durationchange', setInitialDuration);
+media.addEventListener('timeupdate', setDuration);
+media.addEventListener('ended', stopMedia);
+
+timeline.addEventListener('input', skipToPosition);
+volume.addEventListener('input', setVolume);
+volumeToggle.addEventListener('click', toggleVolume);
+
+const initPictureInPicture = () => {
+    pip.addEventListener('click', () => {
+        if (!document.pictureInPictureElement) {
+            media.requestPictureInPicture();
+        } else {
+            document.exitPictureInPicture();
+        }
+    });
+    media.addEventListener('enterpictureinpicture', () => {
+        console.log('Entered PiP');
+    });
+    media.addEventListener('leavepictureinpicture', () => {
+        console.log('Left PiP');
+    });
+}
+
+if ('pictureInPictureEnabled'  in document) {
+    initPictureInPicture();
+}
